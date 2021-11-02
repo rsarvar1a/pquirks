616d3d216d001d5662a160389a54a92750e2f147
rsarvar1a <rsarvaria@uwaterloo.ca>
2021-11-02 12:47:03 -0400

    fix(*): conflicting macro definition

--- src/base/Macro.h
+++ src/base/Macro.h
@@ -3,9 +3,9 @@
 #define PQ_MACRO_H_
 
 #define stx(x) #x
-#define str(x) stx(x)
-#define inc(x) str(x/x.h)
-#define rule(x) str(../x/x.h)
+#define stringify(x) stx(x)
+#define inc(x) stringify(x/x.h)
+#define rule(x) stringify(../x/x.h)
 
 #endif
 
